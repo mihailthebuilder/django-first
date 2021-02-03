0956971c4fb7cc252ad223453808ba335d86a65d
Mihail Marian <mihailmarian12@gmail.com>
2021-02-03 19:40:02 +0000

    added voting capability

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 
 def index(request):
@@ -16,9 +17,25 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of the question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {"question": question, "error_message": "You didn't select a chioce."},
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
